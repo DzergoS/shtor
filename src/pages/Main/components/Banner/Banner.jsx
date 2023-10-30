e640061a0b0cca32eece1b532b55beade652b866
Dzergo <t3d*devpass>
2023-10-30 09:10:38 +0000

    add flexo

--- src/pages/Main/components/Banner/Banner.jsx
+++ src/pages/Main/components/Banner/Banner.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {isMobile} from "utils/isMobile";
 
 import banner from 'assets/banner.png';
-import bannerMobile from 'assets/bannerMobile.png';
+import bannerMobile from 'assets/bannerFullSize.jpg';
 
 import './Banners.css';
 
 const Banner = () => {
   return (
-      <img src={isMobile ? bannerMobile : banner} className="banner-img" alt="banner"/>
+      <div className="banner__container">
+        <img src={isMobile ? bannerMobile : banner} className="banner-img" alt="banner"/>
+      </div>
   );
 };
 
