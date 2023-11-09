a391418829f08b034b5c6a16bc626cd0cc74696a
Ez4Real <butilka05roma@gmail.com>
2023-11-10 00:10:09 +0200

    set up forest-admin

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,19 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const { createAgent } = require('@forestadmin/agent');
+const { createMongooseDataSource } = require('@forestadmin/datasource-mongoose');
+createAgent({
+  authSecret: process.env.FOREST_AUTH_SECRET,
+  envSecret: process.env.FOREST_ENV_SECRET,
+  isProduction: process.env.NODE_ENV === 'production',
+
+})
+  .addDataSource(createMongooseDataSource(db))
+  .mountOnExpress(app)
+  .start();
+
+
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+// Define the common properties for all products
+const baseProductSchema = new mongoose.Schema({
   en_name: String,
   ua_name: String,
   group: String,
@@ -9,12 +10,37 @@ const productSchema = new mongoose.Schema({
   en_price: Number,
   ua_price: Number,
   img: {
-        data: Buffer,
-        contentType: String,
-        filename: String
-    }
+    // data: Buffer,
+    contentType: String,
+    filename: String,
+  },
+});
+const BaseProduct = mongoose.model('BaseProduct', baseProductSchema);
+
+const normalProductSchema = new mongoose.Schema({
+  photos: [String],
+  priceRiseWithChain: Boolean,
+});
+const NormalProduct = BaseProduct.discriminator('NormalProduct', normalProductSchema);
+
+
+const braceletSchema = new mongoose.Schema({
+  sizes: [String], 
+  colorChoices: [String], 
+  priceStable: Boolean,
+});
+const Bracelet = BaseProduct.discriminator('Bracelet', braceletSchema);
+
+const shellSchema = new mongoose.Schema({
+  sizes: [String],
+  priceDependsOnSize: Boolean,
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Shell = BaseProduct.discriminator('Shell', shellSchema);
 
-module.exports = Product;
+module.exports = {
+  BaseProduct,
+  NormalProduct,
+  Bracelet,
+  Shell,
+};
