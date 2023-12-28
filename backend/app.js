2f7aeb2c14dd11ca63eebb0689d7e9c4449ce709
Ez4Real <butilka05roma@gmail.com>
2023-12-28 21:23:10 +0200

    bundle build

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+  path = require('path'),
   HTTP = require('http'),
   cors = require('cors'),
   cookieParser = require('cookie-parser'),
@@ -6,6 +7,7 @@ const express = require('express'),
   genericRouter = require('./routes/index');
 
 require('dotenv').config();
+
 
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.HOSTNAME;
@@ -26,6 +28,13 @@ const startServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser(process.env.COOKIE_SECRET));
   app.use('/', genericRouter);
+
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Handle other routes by serving the index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 
   const db = await connectToDB();
   const server = HTTP.createServer(app);
