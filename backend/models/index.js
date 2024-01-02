8961c1bc151b6677e78d76eb86443c93d6d9c65d
Dzergo <t3d*devpass>
2024-01-02 19:27:57 +0000

    add new build

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,9 @@
 const User = require('./Users/User')
 const Product = require('./Products/Product');
-const Image = require('./Params/Image');
 const Order = require('./Orders/Order');
 
 module.exports = {
 	User,
 	Product,
-	Image,
 	Order,
 }
