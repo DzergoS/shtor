03b463c1bbd467c8f49ba9addf90c8368526651d
Dzergo <t3d*devpass>
2024-01-04 16:24:38 +0000

    add new build mobile photos

--- frontend/src/utils/getProduct.js
+++ frontend/src/utils/getProduct.js
@@ -1,8 +1,7 @@
 export const getProductName = (product, lang) => product?.name?.[lang] || product.variations[0].name
 
-export const getProductImageName = (product) => {
-	return product?.variations?.[0]?.images?.[0] || product?.images?.[0] || product?.seashells?.[0]
-}
+export const getProductImageName = (product) =>
+	product?.variations?.[0]?.images?.[0] || product?.images?.[0] || product?.seashells?.[0]?.[0]
 
 export const getProductImageNameHover = (product) => product?.variations?.[0]?.images?.[1] || product?.images?.[1]
 	|| product?.variations?.[0]?.images?.[0] || product?.images?.[0]
