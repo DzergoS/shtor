f200ad4772090f2dbba52ba02bae6f80bbb7bcab
Ez4Real <butilka05roma@gmail.com>
2024-01-13 18:16:01 +0200

    add Subcriber model && add Subcriber email validation

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./Users/User')
 const Product = require('./Products/Product');
 const Order = require('./Orders/Order');
+const Subscriber = require('./Subscribers/Subscriber');
 
 module.exports = {
 	User,
 	Product,
 	Order,
+	Subscriber
 }
--- frontend/src/ui-components/Footer/Footer.jsx
+++ frontend/src/ui-components/Footer/Footer.jsx
@@ -4,11 +4,29 @@ import {Link} from "react-router-dom";
 import FooterLogo from "assets/logo-footer.jpg";
 import FooterIcon from "assets/arrow-email.svg";
 import './Footer.css';
-import {PaymentAndDeliveryTitle, PrivacyPolicyTitle, ReturnsAndExchangeText, ReturnsAndExchangeTitle} from "../../info";
-import useAPI from "../../provider/useAPI";
 import FooterLinks from "../FooterLinks";
 
 const Footer = () => {
+	const [email, setEmail] = useState('');
+  	const [error, setError] = useState(null);
+
+	function isValidEmail(email) {
+		return /\S+@\S+\.\S+/.test(email);
+	}
+
+	const handleChange = (event) => {
+		const inputEmail = event.target.value;
+		let newError = null;
+	  
+		if (inputEmail.trim() !== '' && !isValidEmail(inputEmail)) {
+		  newError = 'Email is invalid';
+		}
+	  
+		setError(newError);
+		setEmail(inputEmail);
+	};
+
+
 	return (
 		<footer className="footer">
 			<div className="footer-logo">
@@ -16,10 +34,19 @@ const Footer = () => {
 					<img src={FooterLogo} alt="footer-main__logo"/>
 				</Link>
 				<p className="footer-title">Stay informed about releases and special events</p>
-				<div className="email-input">
-					<input className="email-send" type="email" placeholder="Email"></input>
-					<img className="icon-img" src={FooterIcon} alt="icon-arrow"/>
-				</div>
+				<form method='POST'>
+					<div className="email-input">
+						<input
+							className="email-send"
+							type="email"
+							placeholder="Email"
+							value={email}
+        					onChange={handleChange}
+						/>
+						<img className="icon-img" src={FooterIcon} alt="icon-arrow"/>
+					</div>
+					{error && <p className='email-input-error'>{error}</p>}
+				</form>
 			</div>
 			<FooterLinks/>
 		</footer>
