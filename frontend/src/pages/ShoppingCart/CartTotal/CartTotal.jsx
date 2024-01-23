d7adfdfe4a49bdefef6d3cc80cf68da8efe56853
Ez4Real <butilka05roma@gmail.com>
2024-01-23 21:47:46 +0200

    add fondy ebmedded payment && define order model and create order

--- frontend/src/pages/ShoppingCart/CartTotal/CartTotal.jsx
+++ frontend/src/pages/ShoppingCart/CartTotal/CartTotal.jsx
@@ -5,33 +5,27 @@ import useAPI from "provider/useAPI";
 import isMobile from "utils/isMobile";
 import {translations} from "info";
 
-const CartTotal = () => {
+const CartTotal = (props) => {
 	const {state: {cart, lang}} = useAPI();
 
-	const sumOfPrices = cart.reduce((accumulator, currentObject) => {
-		return accumulator + (currentObject.price[lang] * currentObject.quantity);
-	}, 0);
+	
 
 	return (
 		<div className="cart-total">
 			{!isMobile
 				? <>
 					<h4 className="cart-total__subtotal">{translations.cart.subTotal[lang]}</h4>
-					<p className="cart-total__total">{translations.cart.total[lang]}: <span>{translations.product.currency[lang]}{sumOfPrices}</span></p>
+					<p className="cart-total__total">{translations.cart.total[lang]}: <span>{translations.product.currency[lang]}{ props.sumOfPrices }</span></p>
 				</>
 				: <>
-					<h4 className="cart-total__subtotal">{translations.cart.subTotal[lang]} <span>{translations.product.currency[lang]}{sumOfPrices}</span></h4>
+					<h4 className="cart-total__subtotal">{translations.cart.subTotal[lang]} <span>{translations.product.currency[lang]}{ props.sumOfPrices }</span></h4>
 					<p className="cart-total__total">{translations.cart.deliveryAlert[lang]}</p>
 				</>
 			}
-			<a
-				href="https://pay.fondy.eu/api/checkout?button=1ebdc2cfb376b9962e96bb9c1caa0da30234ad55"
-				className="f-p-b cart-total__check-out"
-				data-button="">
-				{translations.cart.checkOut[lang]}
-			</a>
+			<button className="cart-total__check-out" onClick={props.checkoutOnClick}>{translations.cart.checkOut[lang]}</button>
 
 		</div>
+
 	);
 };
 
