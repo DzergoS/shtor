c4eb59868ab602fce164e3e00f59dbfb7d086f7d
Ez4Real <butilka05roma@gmail.com>
2024-01-28 23:57:39 +0200

    add send tracking id (template, logic, localization) && fix send order details endpoint path

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,10 +2,10 @@ const express = require('express'),
 	orderRouter = express.Router(),
 	// axios = require('axios'),
 	orderController = require('../controllers/order'),
-	// sendResponse = require('../utils/response'),
+	sendResponse = require('../utils/response'),
 	{ HOSTNAME, PORT } = require('../config'),
 	{ Order } = require('../models'),
-	{ sendOrderDetails } = require('../services/email');
+	{ sendOrderDetails, sendTrackingId } = require('../services/email');
 
 
 const HOST = HOSTNAME === 'localhost' ? `${HOSTNAME}:${PORT}` : HOSTNAME
@@ -14,7 +14,7 @@ const PROTOCOL = HOSTNAME === 'localhost' ? 'http' : 'https';
 
 orderRouter.post('/create', orderController.createOrder)
 
-orderRouter.post('/send', async (req, res) => {
+orderRouter.post('/send-order-details', async (req, res) => {
 	const order = await Order.findOneAndUpdate(
 		{ order_id: req.body.order_id },
 		{ $set: {
@@ -45,18 +45,32 @@ orderRouter.post('/send', async (req, res) => {
     // }
     // try {
     //     const response = await axios.post('https://pay.fondy.eu/api/status/order_id', requestData)
-
 	// 	console.log(response);
-
 	// 	if (response.order_status === 'approved') {
-			
-			
     //     return sendResponse(res, 200, true, responseData, 'Payment status checked successfully')
 	// 	}
 	// 	return sendResponse(res, 500, false, {}, 'Error: Payment not approved')
     // } catch (error) {
     //     return sendResponse(res, 500, false, {}, `Error: ${error}`)
     // }
+})
+
+
+orderRouter.post('/send-tracking-id', async (req, res) => {
+	const requestData = {
+		email: req.body.email,
+		language: req.body.language,
+		tracking_id: req.body.tracking_id,
+		shipping_type: req.body.shipping_type,
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+    }
+
+	try {
+		await sendTrackingId(requestData)
+	} catch (err) {
+		return sendResponse(res, 500, false, {}, `Error sending tracking id: ${err}`)
+	}
 })
 
 module.exports = orderRouter
--- backend/services/email.js
+++ backend/services/email.js
@@ -5,12 +5,12 @@ const fs = require('fs').promises,
       { HOSTNAME, PORT,
         EMAIL_HOST, EMAIL_PORT,
         EMAIL_USER, EMAIL_PASS } = require('../config'),
-      { generateActivationToken } = require('../utils/tokens');
+      { generateActivationToken } = require('../utils/tokens')
 
 
 
 const HOST = HOSTNAME === 'localhost' ? `${HOSTNAME}:${PORT}` : HOSTNAME
-const PROTOCOL = HOSTNAME === 'localhost' ? 'http' : 'https';
+const PROTOCOL = HOSTNAME === 'localhost' ? 'http' : 'https'
 
 const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
@@ -20,11 +20,11 @@ const transporter = nodemailer.createTransport({
     user: EMAIL_USER, 
     pass: EMAIL_PASS,
   },
-});
+})
 
-const emailTemplatesDir = path.join(__dirname, '../templates/email');
-const logoImagePath = path.join(__dirname, '../images/logo.jpg');
-const ProductImagePath = path.join(__dirname, '../../productPhotos/');
+const emailTemplatesDir = path.join(__dirname, '../templates/email')
+const logoImagePath = path.join(__dirname, '../images/logo.jpg')
+const ProductImagePath = path.join(__dirname, '../../productPhotos/')
 
 
 function getMailOptions(
@@ -48,40 +48,40 @@ function productImagesAttachment(products) {
   return products.map((product, index) => ({
     path: path.join(ProductImagePath, product.image),
     cid: `product_${index}`
-  }));
+  }))
 }
 
 
 async function sendActivationEmail(userId, email) {
-  const activationTemplate = path.join(emailTemplatesDir, 'activateSubscription.ejs');
-  const templateContent = await fs.readFile(activationTemplate, 'utf-8');
+  const activationTemplate = path.join(emailTemplatesDir, 'activateSubscription.ejs')
+  const templateContent = await fs.readFile(activationTemplate, 'utf-8')
 
-  const activationToken = generateActivationToken(userId);
-  const activationLink = `${PROTOCOL}://${HOST}/api/subscribe/activate/${encodeURIComponent(userId)}/${encodeURIComponent(activationToken)}`;
+  const activationToken = generateActivationToken(userId)
+  const activationLink = `${PROTOCOL}://${HOST}/api/subscribe/activate/${encodeURIComponent(userId)}/${encodeURIComponent(activationToken)}`
   
-  const htmlContent = ejs.render(templateContent, { email, activationLink });
+  const htmlContent = ejs.render(templateContent, { email, activationLink })
 
   const mailOptions = getMailOptions(
     email, htmlContent,
     'Activate your subscription for shtor.com.ua'
   )
 
-  await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions)
 }
 
 
 async function sendOrderDetails(language, email, products, currency, amount, shipping_price) {
-  const templateName = language === 'uk' ? 'uk.ejs' : 'en.ejs';
-  const orderDetailsTemplate = path.join(emailTemplatesDir, '/orderDetails/', templateName);
-  const templateContent = await fs.readFile(orderDetailsTemplate, 'utf-8');
+  const templateName = language === 'uk' ? 'uk.ejs' : 'en.ejs'
+  const orderDetailsTemplate = path.join(emailTemplatesDir, '/orderDetails/', templateName)
+  const templateContent = await fs.readFile(orderDetailsTemplate, 'utf-8')
   
-  const total_products = products.reduce((sum, product) => sum + product.quantity, 0);
+  const total_products = products.reduce((sum, product) => sum + product.quantity, 0)
   const currency_symbol = currency === 'UAH' ? '₴' : '$'
 
   const htmlContent = ejs.render(
     templateContent,
     { email, products, total_products, amount, currency_symbol, shipping_price }
-  );
+  )
 
   const mailOptions = getMailOptions(
     email, htmlContent,
@@ -89,11 +89,42 @@ async function sendOrderDetails(language, email, products, currency, amount, shi
     productImagesAttachment(products)
   )
 
-  await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions)
 }
+
+
+async function sendTrackingId({
+  email, language, tracking_id,
+  shipping_type, first_name, last_name,
+}) {
+  const templateName = language === 'uk' ? 'uk.ejs' : 'en.ejs'
+  const trackingIdTemplate = path.join(emailTemplatesDir, '/clientTrackingId/', templateName)
+  const templateContent = await fs.readFile(trackingIdTemplate, 'utf-8')
+
+  const email_subject = language === 'uk' 
+        ? 'Відстеження номеру вашого замовлення'
+        : 'Tracking Information for Your Package'
+  const shipping_link = shipping_type === 'Ukraine'
+        ? 'https://tracking.novaposhta.ua/'
+        : 'https://www.ups.com/' 
+
+  const htmlContent = ejs.render(
+    templateContent,
+    { first_name, last_name, tracking_id, shipping_link }
+  )
+
+  const mailOptions = getMailOptions(
+    email, htmlContent, email_subject,
+  )
+
+
+  await transporter.sendMail(mailOptions)
+}
+
 
 
 module.exports = {
   sendActivationEmail,
-  sendOrderDetails
+  sendOrderDetails,
+  sendTrackingId
 }
