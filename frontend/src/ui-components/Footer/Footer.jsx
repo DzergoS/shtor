db1f3edd2ef3a89ec7192d2f9563ba4a86a37ce5
Dzergo <t3d*devpass>
2024-01-29 16:40:37 +0000

    add build

--- frontend/src/ui-components/Footer/Footer.jsx
+++ frontend/src/ui-components/Footer/Footer.jsx
@@ -1,12 +1,11 @@
 // Header.js
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
-import FooterLogo from "assets/logo-footer.jpg";
-import FooterIcon from "assets/logo-black.png";
+import FooterLogo from "assets/logo-black.png";
+import FooterIcon from "assets/arrow-email.svg";
 import './Footer.css';
 import FooterLinks from "../FooterLinks";
 import api from "../../api";
-import logo from 'assets/logo.png';
 
 
 const Footer = () => {
@@ -52,7 +51,7 @@ const Footer = () => {
 		<footer className="footer">
 			<div className="footer-logo">
 				<Link to="/" className='homeBtn'>
-					<img src={logo} alt="footer-main__logo"/>
+					<img src={FooterLogo} alt="footer-main__logo"/>
 				</Link>
 				<p className="footer-title">Stay informed about releases and special events</p>
 				<form method='POST' onSubmit={onSubmit}>
