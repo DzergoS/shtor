a156a45b2836d8733bc2bc66aabb49aaf4a07c32
Dzergo <t3d*devpass>
2024-02-06 10:07:43 +0000

    add build

--- frontend/src/provider/APIProvider.js
+++ frontend/src/provider/APIProvider.js
@@ -9,6 +9,9 @@ import {CHANGE_IS_MOBILE} from "./actions/mobile";
 import currencyReducer from "./reducers/currency";
 import api from "../api";
 import {ADD_PRODUCTS} from "./actions/products";
+import axios from "axios";
+import {CHANGE_LANG} from "./actions/lang";
+import {CHANGE_CURRENCY} from "./actions/currency";
 
 const APIProvider = (props) => {
 
@@ -31,6 +34,38 @@ const APIProvider = (props) => {
 			isMobile: mediaQuery.matches,
 		}
 	);
+
+	useEffect(() => {
+		const fetchUserCountry = async () => {
+			try {
+				const {data: {country_code}} = await axios.get('https://ipapi.co/json/');
+				const euroCountries = ['AT', 'BE', 'CY', 'EE', 'FI', 'FR', 'DE', 'GR', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PT', 'SK', 'SI', 'ES'];
+				if (euroCountries.includes(country_code)) {
+					dispatch({
+						type: CHANGE_LANG,
+						payload: 'en',
+					})
+					dispatch({
+						type: CHANGE_CURRENCY,
+						payload: 'eur',
+					})
+				} else if (country_code !== 'UA') {
+					dispatch({
+						type: CHANGE_LANG,
+						payload: 'en',
+					})
+					dispatch({
+						type: CHANGE_CURRENCY,
+						payload: 'usd',
+					})
+				}
+			} catch (error) {
+				console.error('Error fetching user country:', error);
+			}
+		};
+
+		fetchUserCountry();
+	}, []);
 
 	const handleTabletChange = (e) => dispatch({
 		type: CHANGE_IS_MOBILE,
