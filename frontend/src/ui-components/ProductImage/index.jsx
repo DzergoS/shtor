c969eb88858e69a64efd0235fd422564a6fe7365
Dzergo <t3d*devpass>
2024-02-12 13:00:57 +0000

    add ph

--- frontend/src/ui-components/ProductImage/index.jsx
+++ frontend/src/ui-components/ProductImage/index.jsx
@@ -3,38 +3,9 @@ import {appURL} from "../../config";
 
 const addFolderLocation = (url) => `${appURL}/productPhotos/${url}`;
 
-// const getFileNameAndExtension = (fullFileName) => {
-// 	const lastDotIndex = fullFileName.lastIndexOf('.');
-// 	const fileName = lastDotIndex !== -1 ? fullFileName.substring(0, lastDotIndex) : fullFileName;
-// 	const fileExtension = lastDotIndex !== -1 ? fullFileName.substring(lastDotIndex + 1) : '';
-//
-// 	return { fileName, fileExtension };
-// }
-
 const ProductImage = ({ imageName = '', ...props }) => {
-	const [imageExists, setImageExists] = useState(true);
 
 	const src = addFolderLocation(imageName);
-
-	// const { fileName, fileExtension } = getFileNameAndExtension(imageName);
-	// const mobileSrc = addFolderLocation(`${fileName}_mobile.${fileExtension}`);
-
-	useEffect(() => {
-		const checkImage = async () => {
-			try {
-				const response = await fetch(src);
-				if (!response.ok) {
-					setImageExists(false);
-				}
-			} catch (error) {
-				setImageExists(false);
-			}
-		};
-
-		checkImage();
-	}, [imageName]);
-
-	// const src = isMobile ? imageExists ? mobileSrc : src : imageExists ? src : mobileSrc
 
 	return <img {...props} className={`${props?.className} ${imageName.replace('.','')}`} src={src} />
 };
