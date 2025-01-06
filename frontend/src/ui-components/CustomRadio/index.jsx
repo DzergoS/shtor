410e409c9ea60221dc79039648d5f245a1f9f7c1
ez4real <butilka05roma@gmail.com>
2025-01-06 15:59:30 -0500

    fix size variations on product

--- frontend/src/ui-components/CustomRadio/index.jsx
+++ frontend/src/ui-components/CustomRadio/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './CustomRadio.css'
 
 const CustomRadio = ({name, value, showValue, checked, onChange, type}) => {
+
+	console.log(`${name} - ${value} - ${showValue}`);
+	
+
 	return (
 		<label className={`custom-radio ${name} ${checked ? 'checked' : ''}`}>
 			<input name={name} checked={checked} type={type || "radio"} value={value} onChange={onChange} hidden/>
 			<div className="custom-radio__switch"/>
-			<span className="custom-radio__value">{name === 'size' ? '' : showValue}</span>
+			<span className="custom-radio__value">{showValue}</span>
 		</label>
 	);
 };
